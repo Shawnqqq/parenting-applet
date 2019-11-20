80782a548b2a1a44b2b8935154ac0f10b5fa8272
Shawnqqq <1206190166@qq.com>
2019-11-20 21:25:56 +0800

    update

--- pages/banner/banner.js
+++ pages/banner/banner.js
@@ -2,16 +2,23 @@ import API from '../../global/request/api.js'
 
 Page({
   data: {
-    bannerData:{}
+    bannerData:""
   },
   onLoad: function (options) {
     wx.request({
       url: API.wxBanner+'/'+options.id,
       success:res=>{
         this.setData({
-          bannerData:res.data.data
+          bannerData:res.data.data.content
         })
       }
     })
+  },
+  onEditorReady() {
+    wx.createSelectorQuery().select('#editor-banner').context(res => {
+      res.context.setContents({
+        html: this.data.bannerData
+      })
+    }).exec()
   }
 })
